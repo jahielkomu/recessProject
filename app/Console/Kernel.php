ffb4d7e6c49a7a3817d345d079f326fcca3af7d6
aksam Lwanga <Lwangaaksam@gmail.com>
2019-07-17 22:54:01 +0300

    caleb is adding the remaining section of this part cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,43 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
-<<<<<<< HEAD
         //         ->hourly();
         $schedule->command('command:addRecord')
         ->everyFiveMinutes();
-=======
-        //          ->hourly();
-       
-        // CHANGES APEAR HERE ONLY
-        $schedule->call(      
-
-
-            function (){
-    
-                $files = Storage::files('/DistrictFiles');
-                foreach($files as $district){
-        
-                $content = Storage::get($district);
-        
-                $contents = explode("\n",$content);
-                    foreach($contents as $arrays){
-                        $name = explode(",",$arrays);
-                        DB::table('members')->updateOrInsert(
-                            ['name'=>$name[0],'gender'=>$name[1],'recommender'=>$name[2],'date'=>$name[3]]
-                        );
-        
-                    }
-        
-                }
-                return '<script>alert("Data inserted check records");</script>';
-            }
-        
-        )->everyFiveMinutes();
-
-
-        
-        
->>>>>>> cf1609b75a235cb0a47f9097316c20a91991d7f8
     }
 
     /**
