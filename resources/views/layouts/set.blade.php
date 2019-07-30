64cfb0a430049cdfd625baf0d30c9da21b14c647
jahielkomu <komuhangitumuhairwe@gmail.com>
2019-07-30 12:49:19 +0300

    DRY

--- resources/views/layouts/set.blade.php
+++ resources/views/layouts/set.blade.php
@@ -58,13 +58,14 @@
                             
         </nav>   
            <!-- /. NAV TOP  -->
-         <!-- <nav class="navbar-default navbar-side" role="navigation">
+        <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse" style="background-color: #999">
                 <ul class="nav" id="main-menu" style="background: #3980b5">
 				<li class="text-center" style="background: #3980b5">
-                    <img src="assets/img/find_user.png" class="user-image img-responsive"/>
+                    <img src="assets/img/logo.png" class="user-image img-responsive"/>
 					</li>
-				    <li style="background-color: rgb(0, 85, 182)">
+
+				    <!--  <li style="background-color: rgb(0, 85, 182)">
                         <a href="/" style="background: #104075;"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
                     </li>
                     <li>
@@ -94,7 +95,7 @@
                
             </div>
             
-        </nav> --> -->
+        </nav> --> 
 
         @yield('required')
 
